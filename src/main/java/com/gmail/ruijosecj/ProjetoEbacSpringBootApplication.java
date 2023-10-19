7f27b53a6bcecaa3ec5d28ce3777cfded9587fa2
ruijosecj <ruijosecj@gmail.com>
2023-10-19 07:31:27 -0300

    commit spring ebac

---
+++ src/main/java/com/gmail/ruijosecj/ProjetoEbacSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.ruijosecj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEbacSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEbacSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/ruijosecj/ProjetoEbacSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.ruijosecj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEbacSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
